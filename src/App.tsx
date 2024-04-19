a2047988736b160f694b16752beda4b43a4730e4
Jorge R. <jorge@programmer.net>
2024-04-19 13:02:04 -0700

    add react query config

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import AppRouter from './router/AppRouter';
+
+
+// Create a client
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppRouter />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
